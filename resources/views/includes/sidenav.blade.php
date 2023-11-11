02b5d3dfee42fd3811e4f0a8b09ed153a6ea23b3
Bagus Pahlefi <baguspahlefi2000@gmail.com>
2023-11-11 21:29:39 +0700

    nav report

--- resources/views/includes/sidenav.blade.php
+++ resources/views/includes/sidenav.blade.php
@@ -13,19 +13,38 @@
                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                 </a>
                 <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                <nav class="sb-sidenav-menu-nested nav">
-                    @if(auth()->user()->accessLevel1())
-                        <a class="nav-link text-white" href="{{route('MoM1')}}">MoM 1 : Daily Meeting Lv 1</a>
-                    @endif
-                    @if(auth()->user()->accessLevel2())
-                        <a class="nav-link text-white" href="{{route('MoM2')}}">MoM 2 : Daily Meeting Lv 2</a>
-                    @endif
-                    @if(auth()->user()->accessLevel3())
-                        <a class="nav-link text-white" href="{{route('MoM3')}}">MoM 3 : Meeting Koordinasi</a>
-                    @endif
-                    
-                </nav>
+                    <nav class="sb-sidenav-menu-nested nav">
+                        @if(auth()->user()->accessLevel1())
+                            <a class="nav-link text-white" href="{{route('MoM1')}}">MoM 1 : Daily Meeting Lv 1</a>
+                        @endif
+                        @if(auth()->user()->accessLevel2())
+                            <a class="nav-link text-white" href="{{route('MoM2')}}">MoM 2 : Daily Meeting Lv 2</a>
+                        @endif
+                        @if(auth()->user()->accessLevel3())
+                            <a class="nav-link text-white" href="{{route('MoM3')}}">MoM 3 : Meeting Koordinasi</a>
+                        @endif
+                    </nav>
                 </div>
+
+                <a class="nav-link collapsed text-white" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayoutsReports" aria-expanded="false" aria-controls="collapseLayouts">
+                    <div class="sb-nav-link-icon"><i class="fas fa-file"></i></div>
+                    Reports
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                    </a>
+                    <div class="collapse" id="collapseLayoutsReports" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                        <nav class="sb-sidenav-menu-nested nav">
+                            @if(auth()->user()->accessLevel1())
+                                <a class="nav-link text-white" href="{{route('MoM1')}}">Meeting Level 1</a>
+                            @endif
+                            @if(auth()->user()->accessLevel2())
+                                <a class="nav-link text-white" href="{{route('MoM2')}}">Meeting Level 2</a>
+                            @endif
+                            @if(auth()->user()->accessLevel3())
+                                <a class="nav-link text-white" href="{{route('MoM3')}}">Meeting Level 3</a>
+                            @endif
+                        </nav>
+                    </div>
+                
                 @role('ADMIN')
                 <a class="nav-link text-white" href="{{route('pengaturanAkun.index')}}">
                     <div class="sb-nav-link-icon"><i class="fas fa-user-alt"></i></div>
